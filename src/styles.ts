30681d6df9fb749a5219e7ed1b232dd56eeaecdd
McomEngine <114884022+McomEngine@users.noreply.github.com>
2024-06-06 06:51:44 +0300

    styles.ts

--- src/styles.ts
+++ src/styles.ts
@@ -25,7 +25,7 @@ export const TosWrapper = styled.div`
   position: relative;
   &:after {
     content: " ";
-    background: linear-gradient(180deg, transparent, #15151f);
+    background: linear-gradient(180deg, transparent, #7affd8);
     height: 50px;
     pointer-events: none;
     width: 100%;
